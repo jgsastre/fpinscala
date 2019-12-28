dfb50dc79222a0a07ffc7a7980e31a01f307b30b
jgarciasastre <jgarciasastre@twilio.com>
2019-12-28 11:20:32 +0100

    Exercise 2.5 Composing

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -195,7 +195,7 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A, B, C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
 
   def main(args: Array[String]): Unit = {
 
